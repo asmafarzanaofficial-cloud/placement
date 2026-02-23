2cb5823601a39a049cf4759bdc14ea01f6dc5e7f
asmafarzanaofficial-cloud <asmafarzana.official@gmail.com>
2026-02-23 12:14:19 +0530

    Create Event.java
    Problem Statement
    Imagine you are developing a program for managing and tracking events hosted at Codissia, a popular event venue. The program should input the number of events hosted at Codissia and display this information.

---
+++ Event.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Event{
+    public static void main(String[]args){
+    Scanner inp=new Scanner(System.in);
+    int e=inp.nextInt();
+    System.out.print("Number of events hosted in Codissia is "+e);
+        
+    }
+}
