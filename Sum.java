1461e9878d8cfaf26cebd6a186b6890204bf38c5
asmafarzanaofficial-cloud <asmafarzana.official@gmail.com>
2026-02-23 12:18:27 +0530

    Create Sum.java
    Problem Statement
    
    Alex is working on a simple financial application to help users calculate the total expenses for a given month. He wants to create a program that reads two integers representing the expenses for two categories and calculates their total. The program should output the result as a double to account for any fractional values that might occur.

---
+++ Sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Sum{
+    public static void main(String[]args){
+        Scanner inpu= new Scanner(System.in);
+        int a=inpu.nextInt();
+        int b=inpu.nextInt();
+        double s=a+b;
+        System.out.print(s);
+    }
+}
