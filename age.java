781dac54535182165f832015f6f462b55cab5273
asmafarzanaofficial-cloud <asmafarzana.official@gmail.com>
2026-02-23 12:16:58 +0530

    Create age.java
    Problem Statement
    
    ﻿John is designing a simple application to display the age of a person along with their name. He wants the program to read the name and age of a person from the user and display the result in the format: "Age of <name> is <age>". This helps in keeping track of different people's ages in a personalized manner.

---
+++ age.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class age{
+    public static void main(String[]args){
+        Scanner inp=new Scanner(System.in);
+        String name=inp.nextLine();
+        int a=inp.nextInt();
+        System.out.print("Age of "+name);
+        System.out.print(" is "+a);
+        }
+}
